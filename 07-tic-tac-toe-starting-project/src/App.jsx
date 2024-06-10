25fc219323a5a4be37a4addb9851edeb88011222
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2024-06-10 13:15:27 +0900

    feat: tic tac toe 게임 화면 틀 잡기

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+          </li>
+          <li></li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
