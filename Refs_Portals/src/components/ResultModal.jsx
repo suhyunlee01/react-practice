68485517df882d876836275759483a9c4cc525fb
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2024-06-12 15:49:36 +0900

    refactor: portal 사용해서 랜더링

--- Refs_Portals/src/components/ResultModal.jsx
+++ Refs_Portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { result, targetTime, timeRemaining, onReset },
@@ -17,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You {result}</h2>}
       {!userLost && <h2>Your Score : {score}</h2>}
@@ -31,7 +32,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
